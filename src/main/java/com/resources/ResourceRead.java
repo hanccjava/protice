b69626f50f9241ee165b1946ca1f37e1abec7274
hanccjava <hanccjava320@126.com>
2018-08-24 15:28:09 +0800

    编辑properties文件

--- src/main/java/com/resources/ResourceRead.java
+++ src/main/java/com/resources/ResourceRead.java
@@ -1,5 +1,7 @@
 package com.resources;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -22,6 +24,22 @@ public class ResourceRead {
         properties.load(inputStream);
         inputStream.close();
         System.out.println(properties.getProperty("cn.welcome.info"));
+
+
+        //ResourceBundle类只能读取内容，如果想要编辑内容，还需要使用properties
+        Properties pro = new Properties();
+        pro.setProperty("bj", "我生存工作的地方");
+        pro.setProperty("yc", "生我养我的地方");
+        pro.setProperty("cq", "我魂牵梦萦想要生活的地方");
+        System.out.println(pro.getProperty("bj"));
+        System.out.println(pro.getProperty("yc"));
+        System.out.println(pro.getProperty("cq"));
+
+        //把属性保存到配置文件中
+        //D:\protice_test\protice\src\main
+        //    pro.store(new FileOutputStream(new File("D:" + File.separator + "protice_test" + File.separator + "protice" + File.separator + "src" + File.separator + "main" + File.separator + "resource" + File.separator + "Han.properties")), "Supplement Info");
+        //可以写绝对路径，和src下写，两种方式
+        pro.store(new FileOutputStream(new File("src" + File.separator + "main" + File.separator + "resource" + File.separator + "Han.properties")), "Supplement Info");
 
     }
 }
